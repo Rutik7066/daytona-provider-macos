7a707d35e5b13222b5cb50bb96cad74b339b1361
Rutik7066 <rutikthakre@gmail.com>
2025-01-28 20:40:28 +0530

    stop target improve
    Signed-off-by: Rutik7066 <rutikthakre@gmail.com>

--- pkg/docker/stop.go
+++ pkg/docker/stop.go
@@ -4,11 +4,9 @@
 package docker
 
 import (
-	"context"
 	"io"
 
 	"github.com/daytonaio/daytona/pkg/models"
-	"github.com/docker/docker/api/types/container"
 )
 
 func (d *DockerClient) StopTarget(target *models.Target, logWriter io.Writer) error {
@@ -16,18 +14,5 @@ func (d *DockerClient) StopTarget(target *models.Target, logWriter io.Writer) er
 	if err != nil {
 		return err
 	}
-
-	err = d.ExecuteCommand("shutdown -h now", logWriter, sshClient)
-	if err != nil {
-		return err
-	}
-
-	containerName := d.GetTargetContainerName(target)
-
-	err = d.apiClient.ContainerStop(context.Background(), containerName, container.StopOptions{})
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return d.ExecuteCommand("shutdown -h now", logWriter, sshClient)
 }
